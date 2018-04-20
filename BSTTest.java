173cc167dedcce6761eafcd94bec5145d3bfb560
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-20 13:39:25 +0700

    install binary search tree

--- BSTTest.java
+++ BSTTest.java
@@ -1,6 +1,5 @@
 public class BSTTest {
     public static void main(String[] args) {
-        //create a BST
         BST<String> tree = new BST<>();
         tree.insert("George");
         tree.insert("Michael");
@@ -9,9 +8,10 @@ public class BSTTest {
         tree.insert("Jones");
         tree.insert("Peter");
         tree.insert("Daniel");
-        //traverse tree
+
         System.out.println("Inorder (sorted): ");
         tree.inorder();
+
         System.out.println("The number of nodes is: " + tree.getSize());
     }
 }
